35b0ec8ce120d456acad97a65b9961a7572e24a2
Stephen Kel <s.kel@student.qut.edu.au>
2018-08-29 13:58:40 +1000

    refactor the code

--- 04_fib_seq.rb
+++ 04_fib_seq.rb
@@ -2,13 +2,12 @@
 
 # 2. There is an odd employee at The Company who needs some code written that will provide them with the first 100 numbers of the Fibonacci sequence ….. (no one really knows why the odd employee needs this information - or why they can’t work it out themselves - but Monday mornings can be strange).
 
-# Initial values
-fib = [0,1]
+
+fib     = [0,1]                                       # Initial fibinacci series
 FIB_NUM = 10                                          # Fib numbers
 
-
 FIB_NUM.times do |index|                              # Loop through fib number of times
-  sum = fib[index+1] + fib[index]
+  sum = fib[index + 1] + fib[index]
   fib.push(sum)                                       # Store sum of previous and current in current index of array
 end
 puts ""
