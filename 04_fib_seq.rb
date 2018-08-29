5bd7f4a18fb9991ccb116ad38138701b9a85b20c
Stephen Kel <s.kel@student.qut.edu.au>
2018-08-29 13:54:30 +1000

    Initial & final

--- 04_fib_seq.rb
+++ 04_fib_seq.rb
@@ -1,3 +1,15 @@
 # 1. sit beside someone new, at a new spot :slightly_smiling_face:
 
 # 2. There is an odd employee at The Company who needs some code written that will provide them with the first 100 numbers of the Fibonacci sequence ….. (no one really knows why the odd employee needs this information - or why they can’t work it out themselves - but Monday mornings can be strange).
+
+# Initial values
+fib = [0,1]
+FIB_NUM = 10                                          # Fib numbers
+
+
+FIB_NUM.times do |index|                              # Loop through fib number of times
+  sum = fib[index+1] + fib[index]
+  fib.push(sum)                                       # Store sum of previous and current in current index of array
+end
+puts ""
+puts fib.inspect                                      # Display fib object
