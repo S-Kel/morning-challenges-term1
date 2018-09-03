ea7fa108d0a8b1d0c8f261ef14d51b832f32d70c
Stephen Kel <s.kel@student.qut.edu.au>
2018-09-03 10:10:12 +1000

    Add answer to challeng

--- 07_fib_seq_01.rb
+++ 07_fib_seq_01.rb
@@ -2,7 +2,7 @@
 
 # The Company has broken the keyboard on their one and only laptop, so can’t get this bit of code to run. What would the output of this code be (without running it on your own computer)? 
 
-#  def fib(num_
+#  def fib(num)
 #   if num < 2
 #      num
 #   else
@@ -10,3 +10,6 @@
 #   end
 # end
 # puts fib(4)
+
+#The output will be: 
+#    [1, 2, (2+1), (3+2)] => [1, 2, 3, 5]
