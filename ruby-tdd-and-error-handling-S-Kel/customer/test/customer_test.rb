e26b8d6a749bcffc40f7e79ed47dcf7a9150b8fc
Stephen Kel <s.kel@student.qut.edu.au>
2018-10-03 10:50:15 +1000

    sync master

--- ruby-tdd-and-error-handling-S-Kel/customer/test/customer_test.rb
+++ ruby-tdd-and-error-handling-S-Kel/customer/test/customer_test.rb
@@ -6,10 +6,10 @@
 
 git checkout master
 git add . && git commit -m "sync master" && git push origin master
-git checkout -b 08_letter_blocks
-git pull --no-edit morning 08_letter_blocks
+git checkout -b 09_broken_keyboard
+git pull --no-edit morning 09_broken_keyboard
 git add . && git commit -m "resolve conflicts"
 git checkout master
-git merge 08_letter_blocks
+git merge 09_broken_keyboard
 git push origin master
-git branch -d 08_letter_blocks
+git branch -d 09_broken_keyboard
