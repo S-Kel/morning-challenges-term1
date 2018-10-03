164e284f8b59060755cca3653a7815b700c6b00e
Stephen Kel <s.kel@student.qut.edu.au>
2018-10-03 10:27:54 +1000

    sync master

--- ruby-tdd-and-error-handling-S-Kel/customer/test/customer_test.rb
+++ ruby-tdd-and-error-handling-S-Kel/customer/test/customer_test.rb
@@ -4,10 +4,10 @@
 
 git checkout master
 git add . && git commit -m "sync master" && git push origin master
-git checkout -b 24_card_game
-git pull --no-edit morning 24_card_game
+git checkout -b 25_deck_of_cards
+git pull --no-edit morning 25_deck_of_cards
 git add . && git commit -m "resolve conflicts"
 git checkout master
-git merge 24_card_game
+git merge 25_deck_of_cards
 git push origin master
-git branch -d 24_card_game
+git branch -d 25_deck_of_cards
