47484585ad26afa6f48fb1e3dee65d91727d347e
Stephen Kel <s.kel@student.qut.edu.au>
2018-10-04 13:30:02 +1000

    coded & tested challenge 21_page_title

--- 21_page_title.rb
+++ 21_page_title.rb
@@ -5,17 +5,24 @@
 # returns the website's title.
 #
 # Examples:
-# pageTitle("http://google.com") => "Google"
-# pageTitle("http://www.reddit.com") => "reddit: the front page of the internet"
+# get_title("http://google.com") => "Google"
+# get_title("http://www.reddit.com") => "reddit: the front page of the internet"
 #
 # Check your answers by running the tests:
 # ruby tests/16_page_title_test.rb
 #
 
-def pageTitle(url)
+def get_title(url)
   # your code here
+  names = url.split('//')[1]
+             .split('.')
+  title = names[1].capitalize
 end
-=======
+
+p get_title("http://www.google.com") 
+p get_title("http://www.reddit.com") 
+p get_title("http://www.reddit.com?name=redit&password=adse123") 
+p get_title("http://www.sudantribune.com/spip.php?article66361")
 #
 # Page Title
 #
@@ -23,8 +30,8 @@ end
 # returns the website's title.
 #
 # Examples:
-# pageTitle("http://google.com") => "Google"
-# pageTitle("http://www.reddit.com") => "reddit: the front page of the internet"
+# get_title("http://google.com") => "Google"
+# get_title("http://www.reddit.com") => "reddit: the front page of the internet"
 #
 # Check your answers by running the tests:
 # ruby tests/16_page_title_test.rb
@@ -33,7 +40,55 @@ require 'nokogiri' # gem install nokogiri (if you don't have it installed)
 require 'open-uri'
 
 def pageTitle(url)
+  # title = get_title(url)
+  # doc = Nokogiri::HTML(open(url))
   # Fetch and parse HTML document
+  
+  # doc.search('title').children[0].text
+  # child = doc.search('title').children[0].text.split(' - ')[1]
+  # child = doc.search('title').children[0].text.split('-')[1].strip
+  #child.to_s.strip!
+  #child
+  # children.each_with_index do |child, i|
+  #   p child.text
+  # end
+end
+
+def pageTitle(url)
+  title = get_title(url)
+  doc = Nokogiri::HTML(open(url))
+
+  case title 
+  when 'Google'
+    # title 
+    doc.search('title').children[0].text
+  when 'Reddit'
+    doc.search('title').children[0].text
+  when 'Sudantribune'
+    doc.search('title').children[0].text.split('-')[1].strip
+  end
+  # Fetch and parse HTML document
+  
+  # doc.search('title').children[0].text
+  # child = doc.search('title').children[0].text.split(' - ')[1]
+  # child = doc.search('title').children[0].text.split('-')[1].strip
+  #child.to_s.strip!
+  #child
+  # children.each_with_index do |child, i|
+  #   p child.text
+  # end
+end
+#p pageTitle("http://www.google.com")
+#p pageTitle("http://www.reddit.com")
+#p pageTitle("http://www.sudantribune.com/spip.php?article66361")
+
+def pageTitle(url)
   doc = Nokogiri::HTML(open(url))
   doc.search('title').children[0].text
 end
+
+p pageTitle("http://www.google.com")
+p pageTitle("http://www.reddit.com")
+p pageTitle("http://www.sudantribune.com/spip.php?article66361")
+
+
--- tests/21_page_title_test.rb
+++ tests/21_page_title_test.rb
@@ -3,9 +3,15 @@ require_relative '../21_page_title'
 
 class IncludedOnceTest < Test::Unit::TestCase
   def test_google
-    assert_equal("Google", pageTitle("http://google.com"))
+    assert_equal("Google", pageTitle("http://www.google.com"))
   end
   def test_facey
     assert_equal("reddit: the front page of the internet", pageTitle("http://www.reddit.com"))
+  end
+  def test_sudantribute
+    title = 
+    "South Sudan main opposition calls for swift release of Machar's spokesperson  - Sudan Tribune: Plural news and views on Sudan"
+
+    assert_equal(title, pageTitle("http://www.sudantribune.com/spip.php?article66361"))
   end
 end
